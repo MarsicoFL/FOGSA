90ad02d3dec1524a379b2aaf365306e094a52964
Franco Marsico <55600771+MarsicoFL@users.noreply.github.com>
2020-03-14 18:25:32 -0300

    Update App.R

--- App.R
+++ App.R
@@ -10,30 +10,34 @@ library(viridis)
 ui <- dashboardPage(skin = "purple",
   dashboardHeader(title = "FOGSA"),
   dashboardSidebar(sidebarMenu(
-                  menuItem("Description", tabName = "Description", icon = icon("th")),
+                  #menuItem("Description", tabName = "Description", icon = icon("")),
                   menuItem("Inputs", tabName = "Inputs", icon = icon("arrow-alt-circle-up")),
                   #menuItem("Pedigri", tabName = "Pedigri", icon = icon("dna")),
-                  menuItem("Descriptive", tabName = "Descriptive statistics", icon = icon("chart-line"),
+                  menuItem("Descriptive statistics", tabName = "Descriptive statistics", icon = icon("chart-line"),
                            menuSubItem("Summary", tabName = "Summary"),
                            menuSubItem("LRdistributions", tabName = "LRdistributions"),
                            menuSubItem("PowerPlot", tabName = "PowerPlot"),
                            menuSubItem("Rates", tabName = "Rates")
                   ),
-                  menuItem("LRdt", tabName = "LRdt", icon = icon("eye")),
-                  sliderInput("LR", "LR thershold:",
-                              min = 1, max = 500,
-                              value = 1, step = 1))
+                  menuItem("LRdt", tabName = "LRdt", icon = icon("eye")))
+                  
   ),
     dashboardBody(
       tabItems(
       # First tab content
-        tabItem(tabName = "Description",
-                h2("FOrensic Genetics Simulation Analysis", align= "Center"),
-        ),
-        
+      # tabItem(tabName = "Description",
       # second tab content
         tabItem(tabName = "Inputs",
-                h2("Uploading data"),
+                h2("FOrensic Genetics Simulation Analysis", align= "Center"),
+                HTML("<h5>Purpose: Despite that the use of DNA in missing person identification has gained increasing focus during the last two decades,  
+                there still are several complications that the forensic scientists are faced with, for instance complex family structures and 
+                large-scale comparisons are examples. Evaluating the impact of these complications in the identification process is one of the 
+                main task of the forensic genetics statistics. The development of algorithms and  the application of statistical analysis in order 
+                to measure the uncertainty and  minimize the probability of both false positive and false negatives identification errors can be crucial
+                to the purpose of the missing person identification.<br/><br/> <b>FOGSA</b> (Forensic Genetics Simulation Analysis)
+                provides an userfriendly interface that allows the analysis of the simulation Raw Data from the Familias software. 
+                FOGSA aim at helping in the statistical analysis for decision making in forensic caseworks.<h5>"),
+                h3("Start uploading your simulation Raw Data"),
                 fluidRow(
                   fileInput("file1", "Familias simulation output",
                             accept = c(
@@ -42,8 +46,7 @@ ui <- dashboardPage(skin = "purple",
                               ".csv")
                   ),
                   tags$hr(),
-                  ),
-        ),
+                  )),
        # third tab content
        tabItem(tabName = "LRdistributions",
                mainPanel(
@@ -66,17 +69,40 @@ ui <- dashboardPage(skin = "purple",
                )),
        # five tab content
        tabItem(tabName = "PowerPlot",
+               h2("Statistical Power Evaluation"),
+               HTML("<h5>For a given pedigree the statistical power of the DNA-based identification could be calculated from the simulated data. Two measures could help to characterize a family group: <br/><br/>
+                  <b>Power of Exclution (PE):</b> It is the probability of obtaining a likelihood ratio of zero when the tested person (POI) is not related with the family group.<br/><br/>
+                  <b>Power of Inclution (PI):</b>The probability of obtaining a likelihood ratio upper than 10000 when the tested person (POI) is actually the missing person (MP).<br/><br/>
+                  The Power Plot could be perfomed combining this two measures. A family group with low PI and PE has low statistical power where a high probability of false positives and false negatives could be expected. In this cases more genetic information must be incorporated to the pedigree. It could be done by the incorporation of more genetic markers or more members.
+                  As couterpart, a pedigree with high PI and PE allows the identification with an accurate result.<br/><br/><br/><h5>"),
                plotOutput(outputId = "PowerPlot")),
        # six tab content
        tabItem(tabName = "Rates",
-               h3("Rates calculation in function of LR threshold selected:"),
-               tableOutput("Rates")),
+               h3("Rates calculation in function of LR threshold selected", align = "Center"),
+               HTML("<h5> False positive and false negative rates could be estimated for each Likelihood ratio selected as threshold (please select one using the sliderbar). 
+               Taking into account this measures we can estimate Matthews Correlation Coefficient and Acuracy. Both measures gives us an idea about how accurate is the LR threshold
+               selected. If you are working with massive comparisons in genetic databases the expected number of false positives could be obtained multiplying the false positive rate 
+               by the database size.<br/><br/><h5>"),
+               tableOutput("Rates"),
+               sliderInput("LR", "LR thershold:",
+                           min = 1, max = 500,
+                           value = 1, step = 1)),
        # seven tab content
        tabItem(tabName = "LRdt",
-               h3("Likelihood Ratio Decision Threshold", align= "Center"),
+               h3("Likelihood Ratio Decision Threshold Plot", align= "Center"),
+               HTML("<h5> <b>Some remarks:</b> In massive comparisons carried on in missing person identification, the matches (LR values upper the LR threshold) are re-analyzed by geneticists and more 
+               genetic (Y-Chromosome, X-Chromosome and mitochondrial DNA) and non-genetic (date of birth) data is incorporated to the case in order to avoid false positives. 
+               In contrast, false negatives are not detected (low LR values) so they don't allow the re-analysis. With this in mind, calculations of true and false negatives rates  
+               with different numbers of LR thresholds (from 1 to 10000) for each family group could be taken into consideration in order to select an appropiate LR threshold. 
+               This value could be used for the establishment of a LR threshold (named LR decision treshold or LRdt)  with the aim of selecting those matches that need a 
+               re-evaluation with more genetic and non genetic data in order to minimize the probability of obtaining a false negative result.<br/><br/>
+               The LR decision threshold should be selected taking into consideration:<br/><br/>
+               <b>    - False negative rate minimization <br/><br/>
+               <b>    - Database size and resources for the re-analysis of the expected number of false positive (false positive rate * database size) <br/><br/>
+               <b>    - The behavior of the LRdt plot. It indicates that in some cases increasing the LRdt value results in a high reduction of false positive rate whilst the false negative rate has a small increase.<br/><h5>"),
+               plotlyOutput("LRdet"),
                h4("X axis: False Negatives Rates"),
-               h4("Y axis: False Positives Rates"),
-               plotlyOutput("LRdet"))
+               h4("Y axis: False Positives Rates"))
       )
     )
   )
@@ -147,7 +173,7 @@ server <- function(input, output) {
     
     plot(density(log10(X2)), 
          xlab= "log10 LR", 
-         ylab= "Densidad", 
+         ylab= "Density", 
          col=1, 
          lwd=2, 
          main="LR distribution - Related" 
@@ -170,7 +196,7 @@ server <- function(input, output) {
     
     plot(density(log10(X1)), 
          xlab= "log10 LR", 
-         ylab= "Densidad", 
+         ylab= "Density", 
          col=2, 
          lwd=2, 
          main="LR distribution - Unrelated" 
@@ -204,6 +230,7 @@ server <- function(input, output) {
          col=2,
          pch = 19,
          lwd=2, 
+         cex=2.5,
          main="Power Plot",
          xlim = c(0,1),
          ylim = c(0,1)
